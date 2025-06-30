89aade66c548e319f774c9175284b5960492e095
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-30 13:47:43 +0300

    fixed up deps

--- config/python.py
+++ config/python.py
@@ -1,10 +1,10 @@
 """ python deps for this project """
 
+import config.shared
+
 install_requires: list[str] = [
     "yq",
 ]
-build_requires: list[str] = [
-    "pydmt",
-    "pymakehelper",
-]
-requires = install_requires + build_requires
+build_requires: list[str] = config.shared.BUILD
+test_requires: list[str] = config.shared.TEST
+requires = install_requires + build_requires + test_requires
